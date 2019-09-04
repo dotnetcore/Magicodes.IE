63c82d474e0c9c58f8a5f4e5f50d4ea7e3bb7af0
zandai <1822802785@qq.com>
2019-09-04 16:45:34 +0800

    修改包命令

--- aa.bat
+++
@@ -1,1 +0,0 @@
-call ./pack.bat "Magicodes.ExporterAndImporter.Core*.nupkg" "./src/Magicodes.ExporterAndImporter.Core/Magicodes.ExporterAndImporter.Core.csproj"
---
+++ packCore.bat
@@ -0,0 +1,2 @@
+call ./pack.bat "Magicodes.IE.Core*.nupkg" "./src/Magicodes.ExporterAndImporter.Core/Magicodes.ExporterAndImporter.Core.csproj"
+@pause
