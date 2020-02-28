9562d65ae8c4d4f25efb4d626df35a318d5402df
HueiFeng <695979933@qq.com>
2020-02-28 18:22:37 +0800

    【新增】增加Excel图片导出

---
+++ src/Magicodes.ExporterAndImporter.Tests/Models/Export/ExportTestDataWithPricture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+
+namespace Magicodes.ExporterAndImporter.Tests.Models.Export
+{
+    [ExcelExporter(Name = "测试",AutoFitAllColumn = true)]
+    public class ExportTestDataWithPicture
+    {
+        [ExporterHeader(DisplayName = "加粗文本", IsBold = true)]
+        public string Text { get; set; }
+        [ExporterHeader(DisplayName = "普通文本")] public string Text2 { get; set; }
+        [ExporterHeader(DisplayName = "忽略", IsIgnore = true)]
+        public string Text3 { get; set; }
+        [ExporterHeader(DisplayName = "数值", Format = "#,##0")]
+        public decimal Number { get; set; }
+        [ExporterHeader(DisplayName = "名称", IsAutoFit = true)]
+        public string Name { get; set; }
+        /// <summary>
+        /// 时间测试
+        /// </summary>
+        [ExporterHeader(DisplayName = "日期1", Format = "yyyy-MM-dd")]
+        public DateTime Time1 { get; set; }
+        [ExporterHeader(DisplayName = "图",IsImg = true,ImgWidth = 50,IsAutoFit = false,ImgHeight = 50)]
+        public string Img { get; set; }
+    }
+}
