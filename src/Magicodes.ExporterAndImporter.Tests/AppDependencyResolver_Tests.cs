c98d4188d0960b0aba51a4c790a3a0dc618015a5
wenqiang.li <codelove1314@live.cn>
2020-09-16 10:37:15 +0800

    添加对AppDependencyResolver的单元测试

---
+++ src/Magicodes.ExporterAndImporter.Tests/AppDependencyResolver_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Core.Filters;
+using Magicodes.ExporterAndImporter.Tests.Models.Import;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Magicodes.ExporterAndImporter.Tests
+{
+    public class AppDependencyResolver_Tests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Fact()]
+        public void AppDependencyResolverNotInit_Test()
+        {
+            Assert.ThrowsAny<System.Exception>(() =>
+            {
+                AppDependencyResolver.Current.GetService<IImportResultFilter>();
+            });
+        }
+
+        [Fact()]
+        public void AppDependencyResolverGetService_Test()
+        {
+            //初始化容器
+            var services = new ServiceCollection();
+            //添加注入关系
+            services.AddSingleton<IImportResultFilter, ImportResultFilterTest>();
+            var serviceProvider = services.BuildServiceProvider();
+            AppDependencyResolver.Init(serviceProvider);
+            AppDependencyResolver.Current.GetService<IImportResultFilter>().ShouldNotBeNull();
+            AppDependencyResolver.Dispose();
+        }
+    }
+}
