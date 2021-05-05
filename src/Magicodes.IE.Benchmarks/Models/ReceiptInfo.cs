4009d8c350b8a8d969fdbeeb77a9dcfff1caced9
Huei Feng <695979933@qq.com>
2021-05-05 21:47:25 +0800

    initial

---
+++ src/Magicodes.IE.Benchmarks/Models/ReceiptInfo.cs
@@ -0,0 +1,74 @@
+﻿using Magicodes.ExporterAndImporter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.Benchmarks.Models
+{
+    [Exporter(Name = "湖南心莱信息科技有限公司电子收款凭证")]
+    public class ReceiptInfo
+    {
+        /// <summary>
+        ///     交易时间
+        /// </summary>
+        public DateTime TradeTime { get; set; }
+
+        /// <summary>
+        ///     姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     身份证
+        /// </summary>
+        public string IdNo { get; set; }
+
+        /// <summary>
+        ///     金额
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        ///     支付方式
+        /// </summary>
+        public string PaymentMethod { get; set; }
+
+        /// <summary>
+        ///     交易状态
+        /// </summary>
+        public string TradeStatus { get; set; }
+
+        /// <summary>
+        ///     备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        ///     年级
+        /// </summary>
+        public string Grade { get; set; }
+
+        /// <summary>
+        ///     专业
+        /// </summary>
+        public string Profession { get; set; }
+
+        /// <summary>
+        ///     收款人
+        /// </summary>
+        public string Payee { get; set; }
+
+        /// <summary>
+        ///     大写金额
+        /// </summary>
+        public string UppercaseAmount { get; set; }
+
+        /// <summary>
+        ///     编号
+        /// </summary>
+        public string Code { get; set; }
+
+    }
+}
