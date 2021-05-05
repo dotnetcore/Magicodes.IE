4e4f98ccbb862b45bfa7c285ae7e88d3deae158c
Huei Feng <695979933@qq.com>
2021-05-05 21:47:25 +0800

    fix #265

---
+++ src/Magicodes.IE.Benchmarks/ExportPdfBenchmarks.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
+using Magicodes.Benchmarks.Models;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Pdf;
+
+namespace Magicodes.Benchmarks
+{
+    //[TailCallDiagnoser]
+    //[EtwProfiler]
+    [SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 5, runtimeMoniker: RuntimeMoniker.NetCoreApp31)]
+    [MemoryDiagnoser]
+    public class ExportPdfBenchmarks
+    {
+        [Params(100, 1000, 10000)]
+        public int RowsCount;
+        private readonly List<ExportTestData> _exportTestData = new List<ExportTestData>();
+        private readonly PdfExporter _exporter;
+        private readonly PdfOptimizeExporter _optimizeExporter;
+        private readonly PdfOptimizeReferenceExporter _optimizeReferenceExporter;
+        private string tpl;
+
+        public ExportPdfBenchmarks()
+        {
+            _exporter = new PdfExporter();
+            _optimizeExporter = new PdfOptimizeExporter();
+            _optimizeReferenceExporter = new PdfOptimizeReferenceExporter();
+
+        }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var tplPath = Path.Combine(Directory.GetCurrentDirectory(), "tpl1.cshtml");
+            tpl = File.ReadAllText(tplPath);
+            for (var i = 1; i <= RowsCount; i++)
+            {
+                _exportTestData.Add(new ExportTestData
+                {
+                    Name1 = "1",
+                    Name2 = "2",
+                    Name3 = "3",
+                    Name4 = "4"
+                });
+            }
+        }
+
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _exportTestData.Clear();
+            tpl = null;
+        }
+
+        [Benchmark]
+        public async Task ExportPdfAsByteArrayTest()
+        {
+            await _exporter.ExportListBytesByTemplate(_exportTestData, tpl);
+        }
+
+        [Benchmark]
+        public async Task ExportPdfOptimizeReferenceAsByteArrayTest()
+        {
+            await _optimizeReferenceExporter.ExportListBytesByTemplate(_exportTestData, tpl);
+        }
+
+        [Benchmark]
+        public async Task ExportPdfOptimizeAsByteArrayTest()
+        {
+            await _optimizeExporter.ExportListBytesByTemplate(_exportTestData, tpl);
+        }
+
+    }
+}
