78f0d2b32c8966eb2d987591f7c4eb30125fda44
xl-wenqiang <wenqiang.li@xin-lai.com>
2022-03-28 20:41:52 +0800

    Add Magicodes.IE.Excel.NPOI

---
+++ src/Magicodes.IE.Excel.NPOI/Extension.cs
@@ -0,0 +1,25 @@
+﻿using NPOI.XSSF.UserModel;
+using System.IO;
+
+namespace Magicodes.IE.Excel.NPOI
+{
+    /// <summary>
+    ///     扩展类
+    /// </summary>
+    public static class Extension
+    {
+        public static byte[] SaveToExcelWithXSSFWorkbook(this byte[] data)
+        {
+            //for excel compability
+            using (var stream = new MemoryStream(data))
+            {
+                XSSFWorkbook wb = new XSSFWorkbook(stream);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    wb.Write(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
