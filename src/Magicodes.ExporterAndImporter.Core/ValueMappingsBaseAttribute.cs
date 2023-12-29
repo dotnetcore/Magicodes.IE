36a0bfb2369f34f0a815cd3839d22e8a3e32e857
netty2019 <1025556487@qq.com>
2023-12-29 16:56:51 +0800

    增加ValueMappingsBase值映射特性基类，方便从外部(如：Abp框架)继承实现枚举、bool类型的多语言显示

---
+++ src/Magicodes.ExporterAndImporter.Core/ValueMappingsBaseAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.IE.Core
+{
+    /// <summary>
+    /// 值映射
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public abstract class ValueMappingsBaseAttribute : Attribute
+    {
+        /// <summary>
+        /// 根据字段类型获取映射
+        /// </summary>
+        /// <param name="fieldType"></param>
+        /// <returns></returns>
+        public abstract Dictionary<string, dynamic> GetMappings(Type fieldType);
+    }
+}
